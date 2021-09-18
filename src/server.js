32eee48ca18e64a386701b80a2726d39a1290f12
wondonghwi <donghwi1289@gmail.com>
2021-09-18 17:27:25 +0900

    Full : front에서 입력한 정보를 서버에 보내주고 서버에서 그걸 그대로 브라우저로 보내주는 채팅 기능 구현 (임시)

--- src/server.js
+++ src/server.js
@@ -14,14 +14,18 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 const handleConnection = socket => {
+  sockets.push(socket);
   console.log('Connected  to Browser ✅');
   socket.on('close', () => console.log('Disconnected from the Browser ❌'));
   socket.on('message', message => {
-    console.log(message.toString('utf8'));
+    // console.log(message.toString('utf8'));
+    sockets.forEach(aSocket => aSocket.send(message.toString('utf8')));
   });
-  socket.send('hello!!!');
 };
+
 wss.on('connection', handleConnection);
 
 server.listen(3000, handleListen);
