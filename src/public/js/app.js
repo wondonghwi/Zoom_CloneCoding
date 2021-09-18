ff0da7290645d97418064b7587719021afc581c8
wondonghwi <donghwi1289@gmail.com>
2021-09-18 18:09:14 +0900

    front : nickname , messageSubmitForm 분리 , Object에 jsonStringfy 적용

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,24 +1,40 @@
 const messageList = document.querySelector('ul');
-const messageForm = document.querySelector('form');
+const nickForm = document.querySelector('#nick');
+const messageForm = document.querySelector('#message');
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+const makeMessage = (type, payload) => {
+  const msg = { type, payload };
+  console.log(msg);
+  return JSON.stringify(msg);
+};
 
 socket.addEventListener('open', () => {
   console.log('Connected to Server ✅');
-});
-
-socket.addEventListener('message', message => {
-  console.log(`New message: ${message.data} from the server`);
 });
 
 socket.addEventListener('close', () => {
   console.log('Disconnected from Server ❌');
 });
 
+socket.addEventListener('message', message => {
+  const li = document.createElement('li');
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
 const handleSubmit = e => {
   e.preventDefault();
   const input = messageForm.querySelector('input');
-  socket.send(input.value);
+  socket.send(makeMessage('new_message', input.value));
   input.value = '';
 };
 
+const handleNickSubmit = e => {
+  e.preventDefault();
+  const input = nickForm.querySelector('input');
+  socket.send(makeMessage('nickname', input.value));
+};
+
 messageForm.addEventListener('submit', handleSubmit);
+nickForm.addEventListener('submit', handleNickSubmit);
