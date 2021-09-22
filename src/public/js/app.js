8b6cb1cc8b4957dca10c00d53e207cfab2289e96
wondonghwi <donghwi1289@gmail.com>
2021-09-22 12:57:35 +0900

    full : Room을 이용한 실시간 대화 / join , left 알림 기능작성

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -8,11 +8,30 @@ room.hidden = true;
 
 let roomName;
 
+const addMessage = msg => {
+  const ul = room.querySelector('ul');
+  const li = document.createElement('li');
+  li.innerText = msg;
+  ul.appendChild(li);
+};
+
+const handleMessageSubmit = e => {
+  e.preventDefault();
+  const input = room.querySelector('input');
+  const value = input.value;
+  socket.emit('new_message', input.value, roomName, () => {
+    addMessage(`you : ${value}`);
+  });
+  input.value = '';
+};
+
 const showRoom = () => {
   welcome.hidden = true;
   room.hidden = false;
   const h3 = room.querySelector('h3');
   h3.innerText = `Room ${roomName}`;
+  const form = room.querySelector('form');
+  form.addEventListener('submit', handleMessageSubmit);
 };
 
 const handleRoomSubmit = e => {
@@ -24,3 +43,13 @@ const handleRoomSubmit = e => {
 };
 
 form.addEventListener('submit', handleRoomSubmit);
+
+socket.on('welcome', () => {
+  addMessage('someone joined!');
+});
+
+socket.on('bye', () => {
+  addMessage('someone left ㅠㅠ');
+});
+
+socket.on('new_message', addMessage);
