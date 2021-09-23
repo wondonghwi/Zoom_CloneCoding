d6dc76c9c54ec2851c7c1dcf2af9936fc2cacec0
wondonghwi <donghwi1289@gmail.com>
2021-09-23 21:57:47 +0900

    front : 카메라가 여러개있을때 선택해서 카메라 바꿀 수 있도록 코드작성

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,10 +13,14 @@ const getCameras = async () => {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter(device => device.kind === 'videoinput');
+    const currentCamera = myStream.getVideoTracks()[0];
     cameras.forEach(camera => {
       const option = document.createElement('option');
       option.value = camera.deviceId;
       option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
       camerasSelect.appendChild(option);
     });
   } catch (e) {
@@ -24,14 +28,21 @@ const getCameras = async () => {
   }
 };
 
-const getMedia = async () => {
+const getMedia = async deviceId => {
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: 'user' },
+  };
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
+    myStream = await navigator.mediaDevices.getUserMedia(deviceId ? cameraConstraints : initialConstrains);
     myFace.srcObject = myStream;
-    await getCameras();
+    if (!deviceId) {
+      await getCameras();
+    }
   } catch (e) {
     console.log(e);
   }
@@ -61,5 +72,10 @@ const handleCameraClick = () => {
   }
 };
 
+const handleCameraChange = async () => {
+  await getMedia(camerasSelect.value);
+};
+
 muteBtn.addEventListener('click', handleMuteClick);
 cameraBtn.addEventListener('click', handleCameraClick);
+camerasSelect.addEventListener('input', handleCameraChange);
