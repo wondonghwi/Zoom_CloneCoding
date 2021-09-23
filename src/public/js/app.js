9bb55b12c2eb154a22708abde3146ee016cc4cfa
wondonghwi <donghwi1289@gmail.com>
2021-09-23 21:12:10 +0900

    full : 웹에서 카메라를 켜고 mute / camera 켜고 끌수있도록 (일단 Text만) 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,46 @@
 const socket = io();
+
+const myFace = document.getElementById('myFace');
+const muteBtn = document.getElementById('mute');
+const cameraBtn = document.getElementById('camera');
+
+let myStram;
+let muted = false;
+let cameraOff = false;
+
+const getMedia = async () => {
+  try {
+    myStram = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    myFace.srcObject = myStram;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+getMedia();
+
+const handleMuteClick = () => {
+  if (!muted) {
+    muteBtn.innerText = 'Unmute';
+    muted = true;
+  } else {
+    muteBtn.innerText = 'Mute';
+    muted = false;
+  }
+};
+
+const handleCameraClick = () => {
+  if (cameraOff) {
+    cameraBtn.innerText = 'Turn Camera Off';
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = 'Turn Camera On';
+    cameraOff = true;
+  }
+};
+
+muteBtn.addEventListener('click', handleMuteClick);
+cameraBtn.addEventListener('click', handleCameraClick);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from 'http';
+import SocketIO from 'socket.io';
 import express from 'express';
 
 const app = express();
@@ -10,6 +11,8 @@ app.get('/', (_, res) => res.render('home'));
 app.get('/*', (_, res) => res.redirect('/'));
 
 const httpServer = http.createServer(app);
+
+const wsServer = SocketIO(httpServer);
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
