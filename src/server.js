44f8937db587fee2000e5bd33acd37ccd345afaa
wondonghwi <donghwi1289@gmail.com>
2021-09-23 20:59:17 +0900

    full : Remove origin Code -> useSocket -> use VideoCall

--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
 import http from 'http';
-import { Server } from 'socket.io';
-import { instrument } from '@socket.io/admin-ui';
 import express from 'express';
 
 const app = express();
@@ -12,60 +10,6 @@ app.get('/', (_, res) => res.render('home'));
 app.get('/*', (_, res) => res.redirect('/'));
 
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer, {
-  cors: {
-    origin: ['https://admin.socket.io'],
-    credentials: true,
-  },
-});
-
-instrument(wsServer, {
-  auth: false,
-});
-
-const publicRooms = () => {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = wsServer;
-  // const sids = wsServer.sockets.adapter.sids;
-  // const rooms = wsServer.sockets.adapter.rooms;
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) publicRooms.push(key);
-  });
-  return publicRooms;
-};
-
-const countRoom = roomName => {
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-};
-
-wsServer.on('connection', socket => {
-  socket.nickname = 'Anon';
-  // socket['nickname'] = 'Anon';
-  socket.onAny(e => {
-    console.log(`Socket Event: ${e}`);
-  });
-  socket.on('enter_room', (roomName, done) => {
-    socket.join(roomName);
-    done();
-    socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName));
-    wsServer.sockets.emit('room_change', publicRooms());
-  });
-  socket.on('disconnecting', () => {
-    socket.rooms.forEach(room => socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1));
-  });
-  socket.on('disconnect', () => {
-    wsServer.sockets.emit('room_change', publicRooms());
-  });
-  socket.on('new_message', (msg, room, done) => {
-    socket.to(room).emit('new_message', `${socket.nickname} : ${msg}`);
-    done();
-  });
-  socket.on('nickname', nickname => (socket.nickname = nickname));
-});
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
