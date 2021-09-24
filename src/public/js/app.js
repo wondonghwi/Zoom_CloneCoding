abcb30c6b888f8c353272f0d84c09c8f24d1ba5d
wondonghwi <donghwi1289@gmail.com>
2021-09-24 09:02:43 +0900

    front : 라이브러리 설치 , 핸드폰으로도 접속할수있도록 변경

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -77,6 +77,11 @@ const handleCameraClick = () => {
 
 const handleCameraChange = async () => {
   await getMedia(camerasSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection.getSenders().find(sender => sender.track.kind === 'video');
+    videoSender.replaceTrack(videoTrack);
+  }
 };
 
 muteBtn.addEventListener('click', handleMuteClick);
