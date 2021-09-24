322cde37fee0cc058a3a8158c78146041d7f34e9
wondonghwi <donghwi1289@gmail.com>
2021-09-24 09:13:48 +0900

    front : 구글에서 제공하는 stun서버 이용

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -142,7 +142,19 @@ socket.on('ice', ice => {
 // RTC Code
 
 const makeConnection = () => {
-  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          'stun:stun.l.google.com:19302',
+          'stun:stun1.l.google.com:19302',
+          'stun:stun2.l.google.com:19302',
+          'stun:stun3.l.google.com:19302',
+          'stun:stun4.l.google.com:19302',
+        ],
+      },
+    ],
+  });
   myPeerConnection.addEventListener('icecandidate', handleIce);
   myPeerConnection.addEventListener('addstream', handleAddStream);
   myStream.getTracks().forEach(track => myPeerConnection.addTrack(track, myStream));
